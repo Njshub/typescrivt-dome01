8e5784eedeee58ce3196d261a5ceefd4fe4def31
Ning_maneong <2415714416@qq.com>
2024-06-03 10:04:51 +0800

    error: failed to push some refs to 'https://github.com/Njshub/typescrivt-dome01.git'

---
+++ index.js
@@ -0,0 +1,12 @@
+// let num: Number = 10
+// let str:String = 'string'
+// let x:any = 'string'
+// let s:any[] = [10,20,'55']
+// let count: number = 20
+// any  什么都能使用
+// const 
+var count = 10;
+console.log(count);
+console.log('homww');
+// console.log(num)
+// console.log(str)
